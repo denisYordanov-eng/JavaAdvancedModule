1af7d451c9df08eeddee6feae6b20ef80e878096
denisYordanov-eng <denisivov437@gmail.com>
2025-10-03 11:29:43 +0300

    Made a task.

---
+++ JavaFundamentals/IntroToJava/Numbers_0To_9_07.java
@@ -0,0 +1,14 @@
+package IntroToJava;
+
+import java.util.Scanner;
+
+public class Numbers_0To_9_07 {
+    public static void main(String[] args) {
+       int number = 0;
+       while(number < 10){
+           System.out.printf("Number: %d\n",number);
+           number++;
+       }
+
+    }
+}
