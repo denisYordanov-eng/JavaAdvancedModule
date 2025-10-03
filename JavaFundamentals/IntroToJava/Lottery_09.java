0b478fce0f0d9c7eeeb9d56c9ef9a16f642f8e13
denisYordanov-eng <denisivov437@gmail.com>
2025-10-03 15:21:29 +0300

    Made a task.

---
+++ JavaFundamentals/IntroToJava/Lottery_09.java
@@ -0,0 +1,15 @@
+package IntroToJava;
+
+
+public class Lottery_09 {
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 8 ; i++){
+            for(int j = i+1; j <= 9 ; j++){
+                for(int k = j+1; k <= 10 ; k++){
+                    System.out.printf("%d %d %d%n", i, j, k);
+                }
+            }
+        }
+    }
+}
