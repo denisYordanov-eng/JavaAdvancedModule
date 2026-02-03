eab4f5c162bc6ed0798931963f4481093a999315
denisYordanov-eng <denisivov437@gmail.com>
2026-02-03 13:21:36 +0200

    Made a task.

---
+++ OOPBasic/inheritance_lab/multylevel_inheritance/Main.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Puppy puppy = new Puppy();
+        puppy.bark();
+        puppy.bark();
+        puppy.weep();
+    }
+}
---
+++ OOPBasic/inheritance_lab/multylevel_inheritance/Puppy.java
@@ -0,0 +1,8 @@
+package inheritance;
+
+
+public class Puppy extends Dog {
+    void weep(){
+        System.out.println("weeping...");
+    }
+}
