bef3efd5a1eb1b0235b5ca44f5fe5759aaa13504
denisYordanov-eng <denisivov437@gmail.com>
2026-02-13 07:35:28 +0200

    Add method for calculating machining time.

--- OOPBasic/personal.Projects/cnc_milling_simulator/LinearCommand.java
+++ OOPBasic/personal.Projects/cnc_milling_simulator/LinearCommand.java
@@ -5,13 +5,25 @@ public class LinearCommand extends BaseCommand {
         super(staringPoint, endPoint, feedRate);
     }
 
-    //todo calculateDistance()
+
+    private double calculateDistance(){
+        double deltaX = getEndPoint().getX() - getStartPoint().getX();
+        double deltaY = getEndPoint().getY() - getStartPoint().getY();
+        double deltaZ = getEndPoint().getZ() - getStartPoint().getZ();
+
+        double sumPow = Math.pow(deltaX,2) + Math.pow(deltaY,2) + Math.pow(deltaZ,2);
+
+        return Math.sqrt(sumPow);
+    }
+
     //todo machiningTime()
+
     @Override
     public String toString() {
-        return String.format("Milling from {%.2f, %.2f, %.2f} to {%.2f, %.2f, %.2f} with feedRate: %.2f"
+        return String.format("Milling from {X:%.2f, Y:%.2f, Z:%.2f} to {X:%.2f, Y:%.2f, Z:%.2f} with feedRate: %.2f," +
+                        " totalDistance: %.2f"
                 , getStartPoint().getX(), getStartPoint().getY(), getStartPoint().getZ()
         ,getEndPoint().getX(),getEndPoint().getY(), getEndPoint().getZ()
-        ,getFeedRate());
+        ,getFeedRate(), calculateDistance());
     }
 }
