876867a8b2d45e03f92a067fad6b628a095174d8
denisYordanov-eng <denisivov437@gmail.com>
2026-04-15 03:48:46 +0300

    Made a task.

---
+++ OOP_Advanced/generics_exercises/custom_list_sorter/Main.java
@@ -0,0 +1,53 @@
+package custom_list_sorter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        CustomList<String> list = new CustomList<>();
+        String line = br.readLine();
+        while (!line.equals("END")) {
+            String[] tokens = line.split("\\s+");
+            switch (tokens[0]) {
+                case "Add":
+                    list.add(tokens[1]);
+                    break;
+                case "Remove":
+                    int index = Integer.parseInt(tokens[1]);
+                    list.remove(index);
+                    break;
+                case "Contains":
+                    System.out.println(list.contains(tokens[1]));
+                    break;
+                case "Swap":
+                    int index1 = Integer.parseInt(tokens[1]);
+                    int index2 = Integer.parseInt(tokens[2]);
+                    list.swap(index1, index2);
+                    break;
+                case "Greater":
+                    System.out.println(list.countGreaterThan(tokens[1]));
+                    break;
+                case "Max":
+                    System.out.println(list.getMax());
+                    break;
+                case "Min":
+                    System.out.println(list.getMin());
+                    break;
+                case "Print":
+                    for (String s : list.getList()) {
+                        System.out.println(s);
+                    }
+                    break;
+                case "Sort":
+                    Sorter.sort(list.getList());
+            }
+            line = br.readLine();
+        }
+    }
+}
