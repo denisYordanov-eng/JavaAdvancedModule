15f7bf947d122bf07b1de86171c40ff9f582f92b
denisYordanov-eng <denisivov437@gmail.com>
2026-04-27 19:35:20 +0300

    Exploring custom annotation.

---
+++ OOP_Advanced/enums_and_annotation/createAnnotation/Main.java
@@ -0,0 +1,13 @@
+package createAnnotation;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+
+    @Subject(categories = {"Test", "Annotation"})
+    public class TestClass {
+
+    }
+}
+
+
