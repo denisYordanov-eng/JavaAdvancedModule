a59258eb65a023a2f43acdb67f5e2929facc19cb
denisYordanov-eng <denisivov437@gmail.com>
2026-05-07 12:39:55 +0300

    Made a task.

---
+++ OOP_Advanced/enums_and_annotation_exercises/reflection_lab/reflection/Main.java
@@ -0,0 +1,21 @@
+package reflection;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+    public static void main(String[] args) throws InstantiationException
+            , IllegalAccessException
+            , NoSuchMethodException
+            , InvocationTargetException {
+        Class<Reflection> cl = Reflection.class;
+        System.out.println(cl);
+        System.out.println(cl.getSuperclass());
+
+        Class<?>[] interfaces = cl.getInterfaces();
+        for (Class<?> anInterface : interfaces) {
+            System.out.println(anInterface);
+        }
+        Reflection reflection =  cl.getDeclaredConstructor().newInstance();
+        System.out.println(reflection);
+    }
+}
