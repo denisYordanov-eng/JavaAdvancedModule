f6db12135c167d8b425f67a96c21e59c876b3dbe
denisYordanov-eng <denisivov437@gmail.com>
2026-05-25 10:03:10 +0300

    Make a task.

---
+++ OOP_Advanced/dependency_inversion_and_interface_segregation_principles/p04_recharge/Employee.java
@@ -0,0 +1,17 @@
+package p04_recharge;
+
+public class Employee extends Worker implements Sleeper {
+
+    public Employee(String id) {
+        super(id);
+    }
+
+    public void sleep() {
+        // sleep...
+        System.out.println("Sleeping");
+    }
+
+//    public void recharge() {
+//        throw new UnsupportedOperationException("Employees cannot recharge");
+//    }
+}
---
+++ OOP_Advanced/dependency_inversion_and_interface_segregation_principles/p04_recharge/Rechargeable.java
@@ -0,0 +1,5 @@
+package p04_recharge;
+
+public interface Rechargeable {
+    void recharge();
+}
